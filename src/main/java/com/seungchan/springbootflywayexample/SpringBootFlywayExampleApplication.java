d6cdbd65ccbbb9af1d2704516df6ebb0bbe6c4b0
Seung-chan <mokdonjr@naver.com>
2019-10-13 15:59:55 +0900

    spring boot project init

---
+++ src/main/java/com/seungchan/springbootflywayexample/SpringBootFlywayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.seungchan.springbootflywayexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlywayExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungchan/springbootflywayexample/SpringBootFlywayExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seungchan.springbootflywayexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFlywayExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
